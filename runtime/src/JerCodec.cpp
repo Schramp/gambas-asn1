1a9092c2aca83942af67e0a132089f807e1ae8f7
Ruud Schramp <ruud@schramp.com>
2026-07-01 07:40:35 +0200

    jer: clarify TODO #165 placement in encode()

--- runtime/src/JerCodec.cpp
+++ runtime/src/JerCodec.cpp
@@ -1037,7 +1037,8 @@ void JerCodec::encode(IEncodeStream& dst,
         prim_dispatch_[def.tag.number]->encode(*this, s, def, src);
     else
         comp_dispatch_[(int)def.kind]->encode(*this, s, def, src);
-    // TODO #165: add validate() call here under ASN1CPP_VALIDATE guards (mirrors BerCodec::encode)
+
+    // TODO #165: add validate() call after encode under ASN1CPP_VALIDATE guards (mirrors BerCodec::encode)
 }
 
 DecodeResult JerCodec::decode(IDecodeStream& src,
